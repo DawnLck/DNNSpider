b49438a05607431a3e5a4b0a2dce3466f144a3d1
LiangChaokai <youchoudeyu@qq.com>
2018-04-09 16:09:36 +0800

    更新content.js

--- js/content.js
+++ js/content.js
@@ -11,9 +11,41 @@ if (jQuery) {
 //     console.log($('body').css('background-color'));
 //     $('body').css('background-color','grey');
 // }, 1000);
-$(document).ready(function (){
-    $('body').css('background-color','#C7EDCC');
+
+/* 标记所有文本节点 */
+function markAllContentDom() {
+    console.log('Mark all content dom ...');
+    $('div').each(function () {
+        console.log($(this).width());
+        if ($(this).width() < 400 || $(this).width() > 1000) {
+            $(this).css('background-color', 'grey');
+            $(this).css('border', 'red solid 2px');
+        }
+        else {
+            console.log('找到目标元素：' + $(this).text());
+            $(this).css('background-color', 'green');
+            $(this).css('border', 'blue solid 2px');
+        }
+    })
+}
+
+/* 标记所有锚节点 */
+function markAllLinkDom() {
+}
+
+$(document).ready(function () {
+        $('body').css('background-color', '#C7EDCC');
     }
 );
+
+
+function contentInit() {
+    setTimeout(function () {
+        markAllContentDom();
+        markAllLinkDom();
+    }, 1000);
+}
+
+contentInit();
 // window.alert('Hello , content js');
 // alert('Hello! My Friend');
