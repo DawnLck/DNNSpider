3bf547518cfd093caebbde55a32b7a69fd285bb8
LiangChaokai <youchoudeyu@qq.com>
2018-12-09 20:46:47 +0800

    完善网页分类的样式

--- src/content/javascripts/global.js
+++ src/content/javascripts/global.js
@@ -1,9 +1,15 @@
 /**
  * Global.js
  * */
-const rootFontSize = parseInt(window.getComputedStyle(document.getElementsByTagName('body')[0]).fontSize),
-  MIN_HEIGHT = 2 * rootFontSize,
-  protocol = document.location.protocol,
+const protocol = document.location.protocol,
   port = protocol === 'https:' ? 8082 : 8081,
   SAVE_PAGE = '/data/saveTestPage',
   SAVE_DOM = '/data/saveTestDom';
+
+const CONFIG = {
+    showClassifyResult: true
+};
+
+const bodyStyle = window.getComputedStyle(document.getElementsByTagName('body')[0]) || {};
+const rootFontSize = parseInt(bodyStyle.fontSize),
+  MIN_HEIGHT = 2 * rootFontSize;
--- src/content/javascripts/modules/webpageClassification.js
+++ src/content/javascripts/modules/webpageClassification.js
@@ -84,12 +84,119 @@ const Classification = {
             '实时'
         ]
     },
-    search:{
+    search: {
         primaryKeys: [
             '搜索'
         ],
         secondKeys: [
+            '百度',
+            '谷歌',
             '记录'
         ]
     }
 };
+
+/* 在网页中显示分类结果 */
+function displayClassifyResult(result) {
+    if (CONFIG.showClassifyResult && result && result.score) {
+        let cateList = '';
+        let cates = result.score || {};
+        for(let key in cates){
+            if(cates.hasOwnProperty(key)){
+                cateList += `<li>[<span>${key}</span>${cates[key]}]</li>`;
+            }
+        }
+        $('body').append(
+          `<div class="spider-toast toast spider-classify"> 
+            <div class="toast-item toast-green">
+                <div class="toast-content"> 
+                  <div class="toast-title">网页类型分析</div> 
+                  <div class="toast-message">
+                    <div class="category">Cate: ${result.category.toUpperCase()}</div>
+                    <ul class="otherCate">${cateList}</ul>
+                  </div> 
+              </div> 
+              <div class="toast-close"></div> 
+            </div>
+           </div>`
+        );
+    }else {
+
+    }
+}
+
+/* 网页分类计算权重 */
+function calculateWeights(count, text, weight) {
+    if (!text) {
+        return count;
+    }
+    for (let key in Classification) {
+        let _tem = Classification[key];
+        for (let j = 0; j < _tem.primaryKeys.length; j++) {
+            if (text.indexOf(_tem.primaryKeys[j]) > -1) {
+                count[key] += (2 * weight);
+            }
+        }
+        for (let x = 0; x < _tem.secondKeys.length; x++) {
+            if (text.indexOf(_tem.secondKeys[x]) > -1) {
+                count[key] += (1 * weight);
+            }
+        }
+    }
+    return count;
+}
+
+/* 网页分类 */
+function pageClassify() {
+    console.info('### DNNSpider - 网页分类 ###');
+
+    const title = document.title,
+      hostname = window.location.hostname,
+      keywords = $('meta[name="keywords"]').attr('content'),
+      description = $('meta[name="description"]').attr('content') || $('meta[name="Description"]').attr('content');
+
+    let result = {};
+
+    for (let key in Classification) {
+        result[key] = hostname.indexOf(key) > -1 ? 1000 : 0;
+    }
+
+    calculateWeights(result, title, 3);
+    calculateWeights(result, keywords, 2);
+    calculateWeights(result, description, 1);
+
+    if ((result.bbs + result.articles + result.news) === 0) {
+        result = calculateWeights(result, $('body').prop('innerText'), 1);
+    }
+
+    let category = 'bbs',
+      categoryValue = result.bbs;
+    for (let key in result) {
+        if (result.hasOwnProperty(key)) {
+            if(result[key] > categoryValue){
+                category = key;
+                categoryValue = result[key];
+            }
+        }
+
+    }
+
+    let output = {
+        title: title,
+        url: window.location.href,
+        domain: hostname.split('.')[1],
+        category: category,
+        meta_keyword: keywords,
+        meta_description: description,
+        score: result
+    };
+
+    displayClassifyResult(output);
+
+    console.log(`Title: ${title}`);
+    console.log(`Keywords: ${keywords}`);
+    console.log(`Description: ${description}`);
+    console.log(`Category: ${category}`);
+
+    return output;
+}
