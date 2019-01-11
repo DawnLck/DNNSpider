19309f76106a44f1ecfefd7281039c765422aeb4
LiangChaokai <youchoudeyu@qq.com>
2019-01-11 16:42:30 +0800

    优化网页分析表格

--- src/content/javascripts/modules/controlPanel.js
+++ src/content/javascripts/modules/controlPanel.js
@@ -1,12 +1,7 @@
 /**
  * ControlPanel 控制面板
  */
-const panelHTML = ` 
-    <div class="spider-control-panel">
-        <h3>网页类型分析</h3>
-        <div id="spider-classify-charts"></div>
-    </div>
-`;
+
 function addControlPanel(pageClassify) {
   console.log("### Show Control Panel ###");
 
@@ -20,16 +15,42 @@ function addControlPanel(pageClassify) {
 
   console.log(classify);
 
+  const panelHTML = ` 
+    <div class="spider-control-panel">
+        <h4>网页类型分析</h4>
+        <h5>该网页属于【${pageClassify.category}】类型</h5>
+        <div id="spider-classify-charts"></div>
+    </div>
+`;
+
   $("body").append(panelHTML);
 
   const chart = new G2.Chart({
     container: "spider-classify-charts",
     width: 280,
-    height: 180
+    height: 180,
+    padding: [50, 10, 50, 10]
   });
+
+  chart.axis("value", false);
 
   chart.source(classify);
   chart.interval().position("cate*value");
-  // .color("genre");
+
+  chart
+    .interval()
+    .position("cate*value")
+    .opacity(1)
+    .label("value", {
+      useHtml: true,
+      htmlTemplate: function htmlTemplate(text, item) {
+        var a = item.point;
+        a.percent = String(parseInt(a.percent * 100)) + "%";
+        return `<span class="g2-label-item"><p class="g2-label-item-value">${
+          a.value
+        }</p></div>`;
+      }
+    });
+
   chart.render();
 }
