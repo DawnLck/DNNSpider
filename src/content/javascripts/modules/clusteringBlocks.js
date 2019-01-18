bcdb64946200b7d0b2c69256d977b2c427b5df79
LiangChaokai <youchoudeyu@qq.com>
2019-01-18 13:15:56 +0800

    距离函数编写（2）

--- src/content/javascripts/modules/clusteringBlocks.js
+++ src/content/javascripts/modules/clusteringBlocks.js
@@ -6,54 +6,68 @@ const DBSCAN_CONFIG = {
     MinPt: 6,
     r = 0.6,
 };
+
+/* 可视块的属性指标 */
+const BlockProperty = {
+    countable: [
+        'childElementCount',
+        'clientHeight',
+        'clientLeft',
+        'clientTop',
+        'clientWidth',
+        'offsetHeight',
+        'offsetLeft',
+        'offsetWidth',
+        'offsetTop',
+        'scrollHeight',
+        'scrollLeft',
+        'scrollTop',
+        'scrollWidth'
+    ],
+    enumerable: [
+        'className',
+        'hidden',
+        'nodeName',
+        'localName',
+        'nodeType',
+        'tagName',
+    ],
+    others: [
+        'innerHTML',
+        'innerText',
+        'outerHTML',
+        'outerText',
+        'textContent',
+    ]
+};
+
+/* 可视块的样式指标 */
+const BlockCss = {
+    countable: [
+        'font-size',
+        'line-height'
+    ],
+    enumerable: [
+        'color',
+        'font-weight',
+        'font-family',
+    ],
+    others: [
+        'content'
+    ]
+}
+
+function distance_countable(a, b){
+    let molecular = Math.pow((a-b), 2);
+    return 1;
+}
+function distance_enumerable(a, b){
+    
+}
 function distance(A, B) {
     let count = 0;
-    const Property = {
-        countable: [
-            'childElementCount',
-            'clientHeight',
-            'clientLeft',
-            'clientTop',
-            'clientWidth',
-            'offsetHeight',
-            'offsetLeft',
-            'offsetWidth',
-            'offsetTop',
-            'scrollHeight',
-            'scrollLeft',
-            'scrollTop',
-            'scrollWidth'
-        ],
-        enumerable: [
-            'className',
-            'hidden',
-            'nodeName',
-            'localName',
-            'nodeType',
-            'tagName',
-        ],
-        others: [
-            'innerHTML',
-            'innerText',
-            'outerHTML',
-            'outerText',
-            'textContent',
-        ]
-    };
-    
-    const Css = {
-        countable: [
-            'font-size',
-            'line-height'
-        ],
-        enumerable: [
-            'color',
-            'font-weight',
-            'font-family',
-        ],
-        others: [
-            'content'
-        ]
+    for(let item of BlockProperty.countable){
+        distance_countable();
     }
 }
 
